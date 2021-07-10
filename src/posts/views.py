46d4173dda6460e4e1392af163203f2f93ce46a4
TaylorShakespear <52114607+poetsec@users.noreply.github.com>
2021-07-09 23:25:19 -0600

    init

---
+++ src/posts/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from .models import Post
+
+# Create your views here.
+
+# CRUD - create retrieve update delete
+
+# List all posts
+
+def post_list_view(request):
+    post_objects = Post.objects.all()
+    context = {
+        "post_objects": post_objects
+    }
+    return render(request, "posts/index.html", context)
